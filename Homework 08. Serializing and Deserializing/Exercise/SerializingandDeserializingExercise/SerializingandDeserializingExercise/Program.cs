fa9b77d632cf016b7a9bf40fd96b842ddd3dab92
MartinTrajanovski <55089636+MartinTrajanovski@users.noreply.github.com>
2020-07-05 20:20:31 +0200

    Serializing and deserializing exercise added

---
+++ Homework 08. Serializing and Deserializing/Exercise/SerializingandDeserializingExercise/SerializingandDeserializingExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using SerializingandDeserializingExercise.Models;
+using SerializingandDeserializingExercise.Services;
+using System;
+using System.Collections.Generic;
+
+namespace SerializingandDeserializingExercise
+{
+    class Program
+    {
+        public static void PrintData(List<Post> posts)
+        {
+            foreach (var post in posts)
+            {
+                if (post.Id < 21)
+                {
+                    Console.WriteLine($"UserId: {post.UserId}\nId: {post.Id}\nTitle: {post.Title}\nBody: {post.Body}");
+                    Console.WriteLine("+++++++++++++++++++++++++++++");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            string userResult = HTTPService.GetData(url);
+
+            List<Post> posts = JsonConvert.DeserializeObject<List<Post>>(userResult);
+
+            PrintData(posts);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework 08. Serializing and Deserializing/Exercise/SerializingandDeserializingExercise/SerializingandDeserializingExercise/Services/HTTPService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace SerializingandDeserializingExercise.Services
+{
+    public class HTTPService
+    {
+        public static string GetData(string url)
+        {
+            using (HttpClient _client = new HttpClient())
+            {
+                HttpResponseMessage response = _client.GetAsync(url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content;
+                    string responseString = responseContent.ReadAsStringAsync().Result;
+
+                    return responseString;
+                }
+                else
+                {
+                    return $"Request failed! Message: {response.RequestMessage}, Status Code: {response.StatusCode}";
+                }
+            }
+        }
+    }
+}
